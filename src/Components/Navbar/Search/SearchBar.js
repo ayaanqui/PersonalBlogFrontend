6834afd475b4987b0967512b014d57ff2fe4791b
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2020-03-22 18:03:47 -0500

    Improved UI, loading, and fail state

--- src/Components/Navbar/Search/SearchBar.js
+++ src/Components/Navbar/Search/SearchBar.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Spinner from 'react-bootstrap/Spinner';
 import axios from "axios";
 
 class SearchBar extends Component {
   state = {
     searchResults: [],
-    showResults: false,
+    loading: false,
+    fail: false,
   }
 
   handleSearchInput = event => {
@@ -12,17 +14,60 @@ class SearchBar extends Component {
     searchInput = searchInput.trim();
 
     if (searchInput !== "") {
+      this.setState({ loading: true });
       axios.get(`http://127.0.0.1:8000/api/posts/?q=${searchInput}&order_by=published&limit=20`)
-        .then(res => {
-          this.setState({ searchResults: res.data });
-        });
+        .then(res => this.setState(
+          {
+            searchResults: res.data,
+            loading: false
+          }
+        ))
+        .catch(err => this.setState(
+          {
+            loading: false,
+            fail: true
+          }
+        ));
+    } else {
+      this.setState(
+        {
+          searchResults: [],
+          loading: false
+        }
+      );
+    }
+  };
+
+  getSearchResults = () => {
+    if (this.state.fail) {
+      return <div className="text-center mt-3"><p>Could not reach API servers</p></div>;
+    } else if (this.state.loading) {
+      return (
+        <div className="text-center mt-3">
+          <Spinner animation="grow" />
+          <p>Fetching results</p>
+        </div>
+      );
+    } else if (this.state.searchResults.length > 0) {
+      return this.state.searchResults.map(post => {
+        return (
+          <a
+            className="dropdown-item font-weight-bold"
+            key={post.id + "_" + post.slug + "_search"}
+          >
+            {post.title}
+          </a>
+        );
+      });
+    } else {
+      return <div className="text-center mt-3"><p>No results found</p></div>;
     }
   };
 
   render() {
     return (
       <React.Fragment>
-        <div className="dropdown" style={{position: "relative"}}>
+        <div className="dropdown" style={{ position: "relative" }}>
           <input
             className="form-control mr-sm-2 form-control-sm"
             id="searchToggle"
@@ -33,22 +78,11 @@ class SearchBar extends Component {
             autoComplete="off"
           />
 
-          <div className="dropdown-menu" aria-labelledby="searchToggle">
+          <div className="dropdown-menu" style={{ right: "8px", left: "auto" }} aria-labelledby="searchToggle">
             <div>
-              <div className="search-results-posts">
-                {this.state.searchResults.map(post => {
-                  return (
-                    <a
-                      className="dropdown-item"
-                      key={post.id + "_" + post.slug + "_search"}
-                    >
-                      {post.title}
-                    </a>
-                  );
-                })}
+              <div className="search-results-posts" style={{ width: "300px" }}>
+                {this.getSearchResults()}
               </div>
-
-              {/* <p>Start typing to see your search results</p> */}
             </div>
           </div>
         </div>
