e8ec62da3bb1c63da4ac4d0b3f7265585da66d71
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2020-03-21 20:55:57 -0500

    Added loading and error status

--- src/Containers/Posts/Posts.js
+++ src/Containers/Posts/Posts.js
@@ -2,40 +2,77 @@ import React, { Component } from 'react';
 import Post from '../../Components/Post/Post';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Spinner from 'react-bootstrap/Spinner';
 import axios from 'axios';
 
 class Posts extends Component {
   state = {
     posts: [],
+    error: false,
+    loading: true,
   }
 
   componentDidMount() {
     axios.get('http://127.0.0.1:8000/api/posts/?order_by=published&limit=9&page=1')
       .then(res => {
         const data = res.data
-        this.setState({ posts: data });
+        this.setState({ posts: data, loading: false });
+      })
+      .catch(error => {
+        this.setState({ error: true, loading: false });
+
       });
   }
 
+  getPosts = () => {
+    if (this.state.loading) {
+      console.log("Loading data");
+      return (
+        <Jumbotron fluid style={{ backgroundColor: "#fff" }}>
+          <Container className="text-center">
+            <Spinner animation="grow" />
+            <p>Loading</p>
+          </Container>
+        </Jumbotron>
+      );
+    } else if (this.state.error) {
+      return (
+        <Jumbotron fluid style={{ backgroundColor: "#fff" }}>
+          <Container className="text-center">
+            <h1 className="font-weight-light">Error Loading Posts</h1>
+            <br />
+            <p>There was an error reaching out to the API. Try refereshing the page.</p>
+          </Container>
+        </Jumbotron>
+      );
+    } else {
+      return (
+        <Row>
+          {this.state.posts.map(post => {
+            return (
+              <Col sm={6} className="mb-3">
+                <Post
+                  title={post.title}
+                  summary={post.summary}
+                  tags={post.tags}
+                  published={post.published}
+                  id={post.id}
+                  slug={post.slug}
+                  key={post.id + "_" + post.slug}
+                />
+              </Col>
+            )
+          })}
+        </Row>
+      );
+    }
+  };
+
   render() {
     return (
-      <Row>
-        {this.state.posts.map(post => {
-          return (
-            <Col sm={6} className="mb-3">
-              <Post
-                title={post.title}
-                summary={post.summary}
-                tags={post.tags}
-                published={post.published}
-                id={post.id}
-                slug={post.slug}
-                key={post.id + "_" + post.slug}
-              />
-            </Col>
-          )
-        })}
-      </Row>
+      this.getPosts()
     );
   }
 }
