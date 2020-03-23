e5679df2cd3cd66f0e5b237a90f3ed64ad4a096e
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2020-03-23 13:06:08 -0500

    Added support for images

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -4,10 +4,30 @@ import Badge from 'react-bootstrap/Badge';
 
 const getRandomHexColor = value => ("#" + Math.floor(Math.random() * value).toString(16));
 
+const getImage = image => {
+  if (image === null) {
+    return (
+      <svg
+        className="bd-placeholder-img card-img-top"
+        width="100%"
+        height="100"
+        role="img"
+      >
+        <title>Placeholder</title>
+        <rect width="100%" height="100%" fill={getRandomHexColor(15777110)} />
+      </svg>
+    );
+  } else {
+    return (
+      <Card.Img variant="top" src={image} />
+    );
+  }
+};
+
 const post = (props) => {
   return (
     <Card>
-      <svg className="bd-placeholder-img card-img-top" width="100%" height="100" role="img"><title>Placeholder</title><rect width="100%" height="100%" fill={getRandomHexColor(15777110)}></rect></svg>
+      {getImage(props.image)}
 
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
